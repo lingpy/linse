d5221dc68afde17097ee1eca7d2644c1ca8413b1
LinguList <mattis.list@posteo.de>
2020-05-01 14:07:48 +0200

    update

--- src/linse/profile.py
+++ src/linse/profile.py
@@ -2,8 +2,6 @@ from collections import defaultdict
 from linse.segment import ipa
 from linse.models import *
 
-__all__ = ['simple', 'context', 'structured']
-
 class Form(object):
 
     def __init__(self, text, **kw):
@@ -15,41 +13,48 @@ class Form(object):
         return str(self.text)
 
 
-def get_profile(
-        *forms, 
-        preceding=None,
-        following=None,
-        segmenter=None,
-        **kw
-        ):
-    """
-    Retrieve a list of graphemes from a list of forms.
-    """
-    if not segmenter:
-        keywords = dict(
-                semi_diacritics='shʃʂɕɦʐʑʒw',
-                merge_vowels=True,
-                merge_geminates=True)
-        keywords.update(kw)
-        segmentize = lambda x: ipa(x['text'], **keywords)
-    else:
-        segmentize = lambda x: segmenter(x['text'], **kw)
-            
-    preceding, following = preceding or '', following or ''
-    graphemes = defaultdict(list)
-    for i, form in enumerate(forms):
-        meta = form.kw if hasattr(form, 'kw') else {'ID': i+1, 'text': form}
-        try:
-            segs = segmentize(meta)
-            segs[0] = preceding+segs[0]
-            segs[-1] += following
-            for segment in segs:
-                if segment:
-                    graphemes[segment, 0].append(meta)
+class DraftProfile(object):
 
-        except Exception as e:
-            graphemes[str(form), str(e)].append(meta)
+    def __init__(self, *forms, preceding=None, following=None, segmenter=None,
+            **kw):
+        """
+        Initialize a draft profile from a list of forms.
+        """
+        # initiate the segmenter
+        if not segmenter:
+            keywords = dict(
+                    semi_diacritics='shʃʂɕɦʐʑʒw',
+                    merge_vowels=True,
+                    merge_geminates=True)
+            keywords.update(kw)
+            self.segmentize = lambda x: ipa(x['text'], **keywords)
+        else:
+            self.segmentize = lambda x: segmenter(x['text'], **kw)
+        # preceding and following
+        self.preceding, self.following = preceding or '', following or ''
+        # counter
+        self.graphemes = defaultdict(list)
+        self.exceptions = defaultdict(list)
+        self.counter = 1
+        # add forms passed upon initialization
+        self.add_forms(*forms)
 
-    return graphemes
+    def add_forms(self, *forms):
+        """
+        Add new forms to the profile.
+        """
+        for i, form in enumerate(forms):
+            meta = form.kw if hasattr(form, 'kw') else {'ID': i+self.counter, 'text': form}
+            try:
+                segs = self.segmentize(meta)
+                segs[0] = self.preceding+segs[0]
+                segs[-1] += self.following
+                for segment in segs:
+                    if segment:
+                        self.graphemes[segment].append(meta)
+            except Exception as e:
+                self.exceptions[str(form), str(e)].append(meta)
+        # increase counter by i
+        self.counter += i
 
 
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,52 +1,59 @@
 import pytest
 
-from linse.profile import Form, get_profile
-
+from linse.profile import Form, DraftProfile 
 
 @pytest.mark.parametrize(
-    'seqs,kw,res',
+    'seqs,kw,res,errs',
     [
         (
           ['khanti'], 
           {}, 
           {'kh': 1, 'a': 1, 'n': 1, 't': 1, 'i': 1},
+          {},
         ),
         (
           ['khanti'], 
           {'preceding': '^', 'following': '$'}, 
           {'^kh': 1, 'a': 1, 'n': 1, 't': 1, 'i$': 1},
+          {},
         ),
 
         (
           ['khantiitta'], 
           {'merge_geminates': True}, 
           {'kh': 1, 'ii': 1, 'tt': 1, 'a': 2, 'n': 1, 't': 1},
+          {},
         ),
         (
           ['kha pt a'],
           {},
-          {'kha pt a': 1}
+          {},
+          {('kha pt a', 'Invalid multi-word string: kha pt a'): 1},
           ),
         (
             ['maggi', None],
             {},
-            {'m': 1, 'a': 1, 'gg': 1, 'i': 1, 'None': 1}            
+            {'m': 1, 'a': 1, 'gg': 1, 'i': 1},
+            {('None', 'Invalid type for word: None'): 1}            
             ),
         (
           ['khantiitta'], 
           {'merge_geminates': False, 'merge_vowels': False, 'semi_diacritics': ''}, 
           {'k': 1, 'h': 1, 'i': 2, 't': 3, 'a': 2, 'n': 1},
+          {}, 
         ),
         (
             [''],
             {},
-            {'': 1}
+            {},
+            {('', 'Invalid empty string: ""'): 1},
             ),
         (
             ['abcde', 'fghij', 'klmn'],
             {"segmenter": list},
             {'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'f': 1, 'g': 1, 'h': 1,
-                'i': 1, 'j': 1, 'k': 1, 'l': 1, 'm': 1, 'n': 1}
+                'i': 1, 'j': 1, 'k': 1, 'l': 1, 'm': 1, 'n': 1},
+            {},
             ),
         (
           ['jeden', 'morgen', 'waŋ', 'wingTsun', 'wàtúvhaks', 'hhhatun'],
@@ -72,14 +79,20 @@ from linse.profile import Form, get_profile
             "vh": 1,
             "ks": 1,
             "hhh": 1,
-           }
+           },
+          {},
           ),
     ]
 )
-def test_get_profile(seqs, kw, res):
-    for (k, check), v in get_profile(*seqs, **kw).items():
+def test_get_profile(seqs, kw, res, errs):
+    for k, v in DraftProfile(*seqs, **kw).graphemes.items():
         assert len(v) == res[k]
-    for (k, check), v in get_profile(*[Form(s) for s in seqs], **kw).items():
+    for k, v in DraftProfile(*[Form(s) for s in seqs], **kw).graphemes.items():
         assert len(v) == res[k]
+
+    for (k, error), v in DraftProfile(*seqs, **kw).exceptions.items():
+        assert len(v) == errs[k, error]
+        
+    
     
 
